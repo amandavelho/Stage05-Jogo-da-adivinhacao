f3bc7c91178861b13a1a89118bdbba6684e9a077
Amanda Velho <amandalimavelho@gmail.com>
2023-10-19 11:53:49 -0300

    finalização do projeto

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
     
+//Variáveis
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2') 
 
 const randomNumber = Math.round(Math.random() * 10)    
 let xAttempts = 1
 
+//Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
 
-//Função callback
+
+//Funções callback
 function handleTryClick(event) {
     event.preventDefault() //Não envie o formulário
 
     const inputNumber = document.querySelector('#inputNumber')
     
-    if (Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        if (Number(inputNumber.value) == randomNumber) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
 
-        document
-            .querySelector('.screen h2').innerText = `acertou em ${xAttempts} tentativas`
+        }
+            inputNumber.value = ''
+            xAttempts++
+}
 
-    }
-    inputNumber.value = ''
-    xAttempts++
-    }
+function handleResetClick(){
+        toggleScreen()
+        xAttempts = 1
+}
 
-
-// Events
-
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-    
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-    xAttempts = 1
-})
+function toggleScreen(){
+        screen1.classList.toggle('hide')
+        screen2.classList.toggle('hide')
+}
